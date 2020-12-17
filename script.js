1b26a4f2a87b1665ff532ec9aa843b1216b7d327
qat47621 <qat47621@gmail.com>
2020-12-16 20:43:09 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,26 @@
+gsap.to('#moon', {
+    scrollTrigger: {
+        scrub: 1
+    },
+    scale: 1.5, 
+})
+gsap.to('#bg', {
+    scrollTrigger: {
+        scrub: 1
+    },
+    scale: 1.1, 
+})
+gsap.to('#santa', {
+    scrollTrigger: {
+        scrub: 1
+    },
+    scale: 1.5,
+    y: -500,
+    x: 2200
+})
+gsap.to('#tree', {
+    scrollTrigger: {
+        scrub: 1
+    },
+    x: -500
+})
